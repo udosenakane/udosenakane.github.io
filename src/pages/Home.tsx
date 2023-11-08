705dca9cfac550c5a7279e9a6e648b18141a180a
Akan Udosen <udosenakane@gmail.com>
2023-11-08 17:57:54 +0100

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,12 +15,12 @@ function Home() {
 
                     <img src={bg} className="w-full" alt="" />
 
-                    <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}
+                    <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
                         className="absolute bg-gray-500 left-0 right-0 
                         top-0 bottom-0 overla overla_1 flex items-center justify-center">
                         <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)' }} className='rounded p-20'>
                             <h1 className='font-bold text-white italics text-lg'>I am Akan Udosen</h1>
-                            <p className='font-bold text-gray-300 italic text-lg my-5'>A FullStack Developer | Cloud Engineer | Technical Support Engineer</p>
+                            <p className='font-bold text-gray-300 italic text-lg my-5'>A FullStack Developer | Cloud Engineer</p>
                         
                             <a href={cv} download className='hover:bg-yellow-600 active:bg-yellow-700 bg-yellow-500 italic rounded p-2'>
                                 Download my CV
